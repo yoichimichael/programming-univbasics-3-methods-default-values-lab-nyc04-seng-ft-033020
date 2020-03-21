59082da6be0a4cfdec9aff26e404de781ba945f6
Yoichi Nagano <yoichi.michael.nagano@gmail.com>
2020-03-21 04:53:28 +0000

    Automatically backed up by Learn

--- lib/meal_choice.rb
+++ lib/meal_choice.rb
@@ -3,5 +3,6 @@
 
 def meal_choice(veg1, veg2, protein = "tofu")
   puts "What a nutritious meal!"
-  p "A plate of #{protein} with #{veg1} and #{veg2}."
+  puts "A plate of #{protein} with #{veg1} and #{veg2}."
+  return "A plate of #{protein} with #{veg1} and #{veg2}."
 end
